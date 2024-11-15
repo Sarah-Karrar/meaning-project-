abe9305fa7ac57cee99e265733ed89d1a64548b2
Sarah-Karrar <123700370+Sarah-Karrar@users.noreply.github.com>
2024-11-15 19:36:52 +0300

    Create python.py
    it's my first project
    starting in python language.

---
+++ python.py
@@ -0,0 +1,12 @@
+meaning = 42
+print('')
+            
+
+# if meaning > 10:
+#     print('Right on!')
+# else: 
+#     print('Not Today')
+
+#Ternary Operator
+print('Right On!') if meaning > 10 else print('Not Today!')
+
